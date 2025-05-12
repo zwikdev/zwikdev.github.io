58233652a09aa970c23b6e20261cbddcca22d3f4
abl0dev <arnold.bloemert@nl.bosch.com>
2025-05-12 15:41:01 +0200

    Improve deployment

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@
 
 ls -al
 
-mkdir -p install-data
+mkdir -p dist/install-data
 
-wget https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-Linux-x86_64.sh -O install-data/Miniforge3-24.3.0-0-Linux-x86_64.sh
-wget https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-MacOSX-x86_64.sh -O install-data/Miniforge3-24.3.0-0-MacOSX-x86_64.sh
-wget https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-Windows-x86_64.exe -O install-data/Miniforge3-24.3.0-0-Windows-x86_64.exe
+cp index.html dist/index.html
 
-cp zwik-client/scripts/zwik-install.sh install-data/zwik-install.sh
-cp zwik-client/scripts/zwik-install.bat install-data/zwik-install.bat
+wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-Linux-x86_64.sh -O dist/install-data/Miniforge3-24.3.0-0-Linux-x86_64.sh
+wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-MacOSX-x86_64.sh -O dist/install-data/Miniforge3-24.3.0-0-MacOSX-x86_64.sh
+wget --progress=dot:mega https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Miniforge3-24.3.0-0-Windows-x86_64.exe -O dist/install-data/Miniforge3-24.3.0-0-Windows-x86_64.exe
 
-cp zwik-client/scripts/zwik_client.py install-data/zwik_client.py
-cp zwik-client/scripts/zwik_client_settings.py install-data/zwik_client_settings.py
+cp zwik-client/scripts/zwik_client.py dist/install-data/zwik_client.py
+cp zwik-client/scripts/zwik_client_settings.py dist/install-data/zwik_client_settings.py
 
-cp zwik-client/bootstrap/zwik_environment zwik_environment.sh
-cp zwik-client/bootstrap/zwik_environment.bat zwik_environment.bat
+cp zwik-client/scripts/zwik-install.sh dist/zwik-install.sh
+cp zwik-client/scripts/zwik-install.bat dist/zwik-install.bat
+
+cp zwik-client/bootstrap/zwik_environment dist/zwik_environment.sh
+cp zwik-client/bootstrap/zwik_environment.bat dist/zwik_environment.bat
